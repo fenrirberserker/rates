3b65cb0b1e1940f952bbbcd0a539305e1c8539e3
fenrirberserker <mlex.alba@gmail.com>
2025-10-01 14:42:47 -0600

    Updating chart, force filtering by specific symbol

--- analytics/src/main/java/com/trader/app/analytics/controller/TradingController.java
+++ analytics/src/main/java/com/trader/app/analytics/controller/TradingController.java
@@ -1,8 +1,10 @@
 package com.trader.app.analytics.controller;
 
 import com.trader.app.analytics.service.EventProducer;
+import com.trader.app.analytics.service.KafkaBridge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.Map;
 
@@ -14,8 +16,18 @@ public class TradingController {
     @Autowired
     private EventProducer eventProducer;
     
+    @Autowired
+    private KafkaBridge kafkaBridge;
+    
     @PostMapping("/event")
     public void sendEvent(@RequestBody Map<String, Object> event) {
         eventProducer.sendTradingEvent(event);
+    }
+    
+    @GetMapping(value = "/events", produces = "text/event-stream")
+    public SseEmitter streamEvents() {
+        SseEmitter emitter = new SseEmitter();
+        kafkaBridge.addEmitter(emitter);
+        return emitter;
     }
 }
--- analytics/src/main/java/com/trader/app/analytics/service/KafkaBridge.java
+++ analytics/src/main/java/com/trader/app/analytics/service/KafkaBridge.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class KafkaBridge {
@@ -14,7 +17,25 @@ public class KafkaBridge {
     @Value("${trading.websocket.destination}")
     private String websocketDestination;
     
+    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    
     public void forwardToWebSocket(Object event) {
         messagingTemplate.convertAndSend(websocketDestination, event);
+        
+        // Also send to SSE clients
+        emitters.removeIf(emitter -> {
+            try {
+                emitter.send(event);
+                return false;
+            } catch (Exception e) {
+                return true;
+            }
+        });
+    }
+    
+    public void addEmitter(SseEmitter emitter) {
+        emitters.add(emitter);
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
     }
 }
